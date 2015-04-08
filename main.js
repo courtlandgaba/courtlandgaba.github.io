d521133028308929ad987c7e003c9f29192835fa
courtland <courtlandgaba128@gmail.com>
2015-04-08 01:44:27 -0400

    commit

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
         responsive: 984,
+        menu: '#myMenu',
+        anchors: ['home', 'about', 'work', 'resume'],
+
     });
 });
