7f3891a215336edca84a85a2024c05d44eb8f832
courtland <courtlandgaba128@gmail.com>
2015-04-10 08:00:56 -0400

    commit

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
         responsive: 984,
+        scrollingSpeed: 600,
         menu: '#myMenu',
         anchors: ['home', 'about', 'work', 'resume'],
 
