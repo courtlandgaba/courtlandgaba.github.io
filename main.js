606d067d9188c41b28f410612f689f3912b02e38
courtland <courtlandgaba128@gmail.com>
2015-08-29 20:55:29 -0400

    fixed page loader

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
+      // HOME PAGE SPECIFIC ITEMS
+    if ( $('body').attr('data-page') === 'home' ) {
+      // Set up PACE options
+      Pace.on('done', function(){
+          $('.cover').fadeOut(2000);
+      });
+
+    }
     $('#fullpage').fullpage({
         responsive: 984,
         scrollingSpeed: 600,
